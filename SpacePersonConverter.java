9b10702c6a9c5892ef4581160fd210eafdea7ab0
ssk5017 <143562412+ssk5017@users.noreply.github.com>
2023-12-07 14:32:07 -0500

    Done?

--- SpacePersonConverter.java
+++ SpacePersonConverter.java
@@ -17,31 +17,31 @@ public class SpacePersonConverter {
         spacePersonAlphabet.put('g', '^');
         spacePersonAlphabet.put('h', '&');
         spacePersonAlphabet.put('i', '*');
-        // Add more mappings for the rest of the alphabet
+        // write mappings for the rest of the alphabet
     }
 
     public static void main(String[] args) {
-        // Prompt the user for the English string
+        // Prompt user for english string
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the English string: ");
         String englishString = scanner.nextLine();
 
-        // Create a new Space Person alphabet
+        // Create brand new Space Person alphabet
         HashMap<Character, Character> newSpacePersonAlphabet = createNewSpacePersonAlphabet();
 
-        // Convert to Space Person string using the new alphabet
+        // Change to Space Person string using a whole new alphabet
         String spacePersonString = convertToSpacePersonString(englishString, newSpacePersonAlphabet);
         System.out.println("Space Person string: " + spacePersonString);
 
-        // Calculate SHA256 hash value for the Space Person string
+        // Calculate SHA256 hash value
         String hashValue = calculateSHA256(spacePersonString);
         System.out.println("SHA256 hash value: " + hashValue);
 
-        // Apply Caesar cipher with a 5-character shift on the English string
+        // Caesar cipher with a 5-character shift (english string)
         String caesarCipherResult = caesarCipher(englishString, 5);
         System.out.println("Caesar cipher (5-character shift): " + caesarCipherResult);
 
-        // Brute force all 0-25 shifts of the English string
+        // Brute force all 0-25 shifts (english string)
         bruteForceCaesarCipher(englishString);
 
         scanner.close();
@@ -49,7 +49,7 @@ public class SpacePersonConverter {
 
     private static HashMap<Character, Character> createNewSpacePersonAlphabet() {
         HashMap<Character, Character> newAlphabet = new HashMap<>();
-        // Use a different set of symbols (~!@#$%^&*) for the new Space Person alphabet
+        // Use a variety set of symbols (~!@#$%^&*) for Space Person alphabet
         char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
         char[] symbols = "~!@#$%^&*".toCharArray();
 
